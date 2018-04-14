052418af5b6294fa4fbf2cace60b41c3266b482e
brainysoon <brainysoon@gmail.com>
2018-04-14 19:20:59 +0800

    [ Work ] Pass should_return_Fizz_when_have_3 test.

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -2,13 +2,22 @@ package com.thoughtworks;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 
 public class FizzBuzzGame {
     private List<String> results = new ArrayList<>();
 
     public void start(int count) {
+        this.results = IntStream.rangeClosed(1, count)
+                .collect(ArrayList::new, (list, value) -> {
 
+                    if (value == 3) {
+                        list.add("Fizz");
+                    } else {
+                        list.add(value + "");
+                    }
+                }, ArrayList::addAll);
     }
 
     public List<String> getResults() {
