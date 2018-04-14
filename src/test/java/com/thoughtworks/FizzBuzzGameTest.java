17f3013cf53e4b3f5fdf06dac136614e52bc96e7
brainysoon <brainysoon@gmail.com>
2018-04-14 19:06:33 +0800

    [ Test ] Add should_return_Fizz_when_have_3 test.

--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -1,10 +1,19 @@
 package com.thoughtworks;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 public class FizzBuzzGameTest {
+
     @Test
     public void should_return_Fizz_when_have_3() {
 
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+        fizzBuzzGame.start(3);
+        List<String> results = fizzBuzzGame.getResults();
+
+        Assert.assertEquals("Fizz", results.get(2));
     }
 }
